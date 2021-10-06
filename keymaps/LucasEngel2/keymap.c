0035d9c2c4010e3cdf3fe9f26a30770a3f3091c0
LDE_Gamer23 <31966529+LucasEngel2@users.noreply.github.com>
2021-10-06 20:53:48 +0200

    update

--- keymaps/LucasEngel2/keymap.c
+++ keymaps/LucasEngel2/keymap.c
@@ -40,8 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
     [_MOD4] = LAYOUT(
-    KC_F1, 	    KC_F2, 		KC_F3, 		KC_F4, 		KC_NO, 		KC_NO, 		KC_BTN1, 	KC_MS_UP, 	KC_BTN2, 	KC_NO,
-    KC_F5,  	KC_F6, 		KC_F7, 		KC_F8, 		KC_NO,   	KC_NO, 		KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,KC_NO,
+    KC_F1, 	    KC_F2, 		KC_F3, 		KC_F4, 		KC_NO, 		KC_NO, 		KC_BTN1, 	KC_MS_U, 	KC_BTN2, 	KC_NO,
+    KC_F5,  	KC_F6, 		KC_F7, 		KC_F8, 		KC_NO,   	KC_NO, 		KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_NO,
     KC_F9, 	    KC_F10, 	KC_F11, 	KC_F12, 	KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
 		        KC_NO, 		KC_NO,      KC_NO, 	    KC_NO, 	    KC_NO, 		KC_NO, 		KC_NO, 		KC_NO
 )
