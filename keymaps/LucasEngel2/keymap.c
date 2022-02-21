ed47386e01ac01979e75f63355f8421d122161d9
LucasEngel2 <loangluci@gmail.com>
2022-02-21 21:44:40 +0100

    update

--- keymaps/LucasEngel2/keymap.c
+++ keymaps/LucasEngel2/keymap.c
@@ -4,12 +4,12 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _DVORAK,
-    _STENO,
-    _GAMING,
-    _NUMPAD,
-    _CODING,
-    _FROW
+	_DVORAK,
+	_STENO,
+	_GAMING,
+	_NUMPAD,
+	_CODING,
+	_FROW
 };
 
 enum combo_events {
@@ -30,6 +30,7 @@ enum unicode_name {
 };
 
 #define ST_GEM QK_STENO_GEMINI
+#define UNICODE_SELECTED_MODES UC_LNX
 
 // Defines your layered keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -48,16 +49,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
     
     [_GAMING] = LAYOUT(
-	KC_TAB, 	DV_Q, 		DV_W, 		DV_E, 		DV_R, 		KC_NO, 		KC_NO, 		KC_NO,	 	KC_NO,		KC_NO,
-	KC_LSFT, 	DV_A, 		DV_S, 		DV_D, 		DV_F, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
+	KC_TAB, 	DV_Q, 		DV_W, 		DV_E, 		DV_R, 		KC_NO, 		KC_BTN1,	KC_MS_U, 	KC_BTN2,	KC_BTN3,
+	KC_LSFT, 	DV_A, 		DV_S, 		DV_D, 		DV_F, 		KC_NO, 		KC_MS_L,	KC_MS_D,	KC_MS_R,	KC_NO,
 	KC_LCTL, 	DV_Z, 		DV_X, 		DV_C, 		DV_V, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
 			DV_1, 		KC_SPC,     	DV_2, 		DV_3, 		TO(_STENO), 	KC_NO, 		KC_NO, 		TO(_DVORAK)
 ),
 
     [_NUMPAD] = LAYOUT(
-	KC_NLCK, 	KC_P7, 		KC_P8, 		KC_P9, 		KC_EXLM, 	KC_NO, 		KC_NO, 		KC_UP, 		KC_NO, 		KC_NO,
+	KC_NLCK, 	KC_P7, 		KC_P8, 		KC_P9, 		KC_EXLM, 	KC_MPLY,	KC_MPRV, 	KC_UP, 		KC_MNXT,	KC_NO,
 	KC_P0, 		KC_P4, 		KC_P5, 		KC_P6, 		KC_CIRC, 	KC_NO, 		KC_LEFT,	KC_DOWN,	KC_RGHT,	KC_LSFT,
-	KC_LALT, 	KC_P1, 		KC_P2, 		KC_P3, 		KC_PERC, 	LCTL(DV_V), 	LCTL(DV_C), 	MO(_FROW), 	LCTL(DV_Z), 	KC_LCTL,
+	KC_LALT, 	KC_P1, 		KC_P2, 		KC_P3, 		KC_PERC, 	KC_PSTE, 	KC_COPY, 	MO(_FROW), 	KC_UNDO, 	KC_LCTL,
 			KC_PPLS,	KC_PMNS,	KC_PAST,	KC_PSLS, 	KC_NO, 		KC_NO, 		KC_NO, 		KC_NO
 ),
 
@@ -73,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     	KC_F1, 	   	KC_F2, 		KC_F3, 		KC_F4, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
 	KC_F5,  	KC_F6, 		KC_F7, 		KC_F8, 		KC_NO,   	KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
 	KC_F9, 	   	KC_F10, 	KC_F11, 	KC_F12, 	KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
-		        KC_NO, 		KC_NO,      	KC_NO, 	    	KC_NO, 	    	KC_NO, 		KC_NO, 		KC_NO, 		KC_NO
+		        KC_NO,		KC_NO,      	KC_NO, 	    	KC_NO, 	    	KC_NO, 		KC_NO, 		KC_NO, 		KC_NO
 )
 
 };
